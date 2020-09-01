527734434fc0691cd1e4310db0f3d146d22fb503
cricketparth <50546286+cricketparth@users.noreply.github.com>
2020-09-01 18:46:41 +0530

    second commit

---
+++ build/precache-manifest.652c5868ec9114e32c088afecf651536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce5b72987d5cc8096ba4e01ca19a54c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ba27c29286a9913c97",
+    "url": "/static/css/main.0772ba7d.chunk.css"
+  },
+  {
+    "revision": "32a8729a25d94378366d",
+    "url": "/static/js/2.c4207cd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4207cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0ba27c29286a9913c97",
+    "url": "/static/js/main.0f875e5a.chunk.js"
+  },
+  {
+    "revision": "baaa98be7cf0bc8168a1",
+    "url": "/static/js/runtime-main.299e455a.js"
+  }
+]);
